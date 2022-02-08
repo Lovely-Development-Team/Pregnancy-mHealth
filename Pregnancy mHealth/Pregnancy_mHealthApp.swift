b0b41856889a9167b231a5249fb2f0f392f2bddc
Ben Cardy <ben.cardy@ocado.com>
2022-02-08 15:03:28 +0000

    Initial Commit

---
+++ Pregnancy mHealth/Pregnancy_mHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pregnancy_mHealthApp.swift
+//  Pregnancy mHealth
+//
+//  Created by Ben Cardy on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pregnancy_mHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
