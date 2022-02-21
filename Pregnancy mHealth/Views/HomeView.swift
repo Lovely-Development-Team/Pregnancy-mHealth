16ff67e76b91576bb9736ef9687ea06ac6b3df83
Ben Cardy <ben.cardy@ocado.com>
2022-02-21 22:15:51 +0000

    Add hacky notifications request button

--- Pregnancy mHealth/Views/HomeView.swift
+++ Pregnancy mHealth/Views/HomeView.swift
@@ -5,13 +5,42 @@
 //  Created by Ben Cardy on 08/02/2022.
 //
 
+
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var notificationsAuthorised: Bool? = nil
+    
     var body: some View {
         NavigationView {
-            Text("Home")
-                .navigationTitle("Home")
+            VStack {
+                Group {
+                if let notificationsAuthorised = notificationsAuthorised {
+                    if notificationsAuthorised {
+                        Text("Notifications Enabled")
+                    } else {
+                        Text("Notifications Not Authorized")
+                    }
+                } else {
+                    Button(action: {
+                        let center = UNUserNotificationCenter.current()
+                        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                            if let error = error {
+                                // Handle error
+                            }
+                            self.notificationsAuthorised = granted
+                        }
+                    }) {
+                        Text("Enable Notifications")
+                    }
+                }
+                }
+                .padding(.top, 20)
+                Spacer()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .navigationTitle("Home")
         }
     }
 }
